4724993b620693de075e0aae8bffdf38eb2ef5bd
SuperSolik <serpetr99@gmail.com>
2023-12-09 13:44:57 +0100

    added wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,34 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.window_background_opacity = 0.95
+
+config.font_size = 16
+
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentTab { confirm = false },
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
