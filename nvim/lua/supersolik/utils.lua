e9f0f729d90aa9225656652d2ba4b5138ba349f9
SuperSolik <serpetr99@gmail.com>
2024-02-20 20:36:35 +0100

    change wrap toggle cmd

--- nvim/lua/supersolik/utils.lua
+++ nvim/lua/supersolik/utils.lua
@@ -1,6 +1,6 @@
 local wrap_is_enabled = false
 
-vim.api.nvim_create_user_command('WrapToggle', function()
+vim.api.nvim_create_user_command('ToggleWrap', function()
     wrap_is_enabled = not wrap_is_enabled
     vim.wo.wrap = wrap_is_enabled
     print('Toggling wrap to: ' .. tostring(wrap_is_enabled))
