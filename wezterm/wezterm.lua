e72d7226a809a9c91c76ba62be0afe86363a0497
SuperSolik <serpetr99@gmail.com>
2024-12-27 00:11:46 +0100

    wezterm upd

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -19,27 +19,30 @@ config.color_scheme = "Zenwritten_dark"
 -- config.window_background_opacity = 1.0
 
 config.font = wezterm.font("JetBrains Mono", { italic = false })
-config.font_size = 16
+config.font_size = 15
 
 wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+    local _, _, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
 end)
 
 config.keys = {
-	{
-		key = "w",
-		mods = "CMD",
-		action = wezterm.action.CloseCurrentTab({ confirm = false }),
-	},
+    {
+        key = "w",
+        mods = "CMD",
+        action = wezterm.action.CloseCurrentTab({ confirm = false }),
+    },
 }
 
 -- disable checking for updates
-config.check_for_updates = false
-config.show_update_window = false
+config.check_for_updates = true
+config.show_update_window = true
 
 -- font
 -- config.font = wezterm.font 'IBM Plex Mono'
+
+-- window decorations
+config.window_decorations = "TITLE | RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
