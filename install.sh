1f12a4f945db74d54599241c6bb8a0c9270ce405
SuperSolik <serpetr99@gmail.com>
2024-12-27 00:13:12 +0100

    trying out ghostty.org

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 ln -v -sf $(readlink -f ./nvim/) $HOME/.config/nvim
 ln -v -sf $(readlink -f ./wezterm/) $HOME/.config/wezterm
+ln -v -sf $(readlink -f ./ghostty/) $HOME/.config/ghostty
 ln -v -sf $(readlink -f ./.ignore) $HOME/.ignore
 ln -v -sf $(readlink -f ./.fzf-key-bindings.zsh) $HOME/.fzf-key-bindings.zsh
