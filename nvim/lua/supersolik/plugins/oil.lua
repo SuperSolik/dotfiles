0f09bce099803ddb1a9fe8866dc3f8f99f3c47e6
SuperSolik <sergeipetrov.dev@icloud.com>
2025-06-24 14:14:04 +0100

    chore: oil config change

--- nvim/lua/supersolik/plugins/oil.lua
+++ nvim/lua/supersolik/plugins/oil.lua
@@ -3,16 +3,28 @@ return {
         "stevearc/oil.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
         lazy = false,
-        opts = {},
-        config = function()
-            require("oil").setup({
-                columns = { "icon" },
-                view_options = {
-                    show_hidden = true,
-                },
-            })
-            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent folder" })
-            vim.keymap.set("n", "<space>-", require("oil").toggle_float)
-        end,
+        keys = {
+            { "-", "<CMD>Oil<CR>", desc = "Open parent folder" },
+            {
+                "<space>-",
+                function()
+                    require("oil").toggle_float()
+                end,
+                desc = "Toggle Oil float",
+            },
+        },
+        opts = {
+            default_file_explorer = true,
+            columns = { "icon" },
+            view_options = {
+                show_hidden = true,
+                is_always_hidden = function(name, _)
+                    -- Return true for files you still want to hide.
+                    -- Example below shows absolutely everything,
+                    -- including "." and ".." and ".git".
+                    return false
+                end,
+            },
+        },
     },
 }
