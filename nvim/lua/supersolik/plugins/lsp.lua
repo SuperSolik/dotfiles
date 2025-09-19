7d566cc58c75a125a19e8efd01d66fd02a67e62a
SuperSolik <serpetr99@gmail.com>
2025-09-19 23:01:36 +0200

    unify lsp remaps to always be prefixed with leader

--- nvim/lua/supersolik/plugins/lsp.lua
+++ nvim/lua/supersolik/plugins/lsp.lua
@@ -42,13 +42,13 @@ return {
                     map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
                     map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
-                    map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-                    map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+                    map("<leader>gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+                    map("<leader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
-                    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-                    map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+                    map("<leader>gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+                    map("<leader>gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-                    map("gt", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+                    map("<leader>gt", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
                     map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
                     map(
